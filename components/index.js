35192e73c7fdfdd6ec97067ba8ea6ed71657856b
theanh031 <theanhnguyen177@gmail.com>
2023-05-30 15:07:26 +0700

    first commit

---
+++ components/index.js
@@ -0,0 +1,11 @@
+import HomeHeader from "./HomeHeader.js"
+import NFTCard from "./NFTCard.js"
+import FocusStatusBar from "./FocusStatusBar.js"
+import { CircleButton, RectButton } from "./Button.js"
+import {SubInfo} from "./SubInfo.js"
+import DetailBid from "./DetailBid.js"
+import DetailDesc from "./DetailDesc.js"
+
+export {
+  HomeHeader, NFTCard, FocusStatusBar, CircleButton, RectButton, SubInfo, DetailBid, DetailDesc
+}
